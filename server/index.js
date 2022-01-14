8b04edac079d01570a1a2630cb2b83417b1ba771
jagdishlove <jagdish.funn@gmail.com>
2022-01-14 12:21:47 +0530

    feat: create server/index.js

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const app = express();
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        method: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`user connected : ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`user with ID:${socket.id} user joined room : ${data}`);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
