b9d08de5ee21e53e60a5dbd0eb72585b6a6a4738
jagdishlove <jagdish.funn@gmail.com>
2022-01-15 17:09:48 +0530

    create 1 file and update 2 files

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
+const http = require("http").createServer(app);
 const app = express();
-const { Server } = require("socket.io");
+const { Server } = require("socket.io").istern(server);
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "window.location.hostname",
         method: ["GET", "POST"],
     },
 });
