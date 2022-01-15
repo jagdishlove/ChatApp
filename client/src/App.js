9817db35683ae394f53e8938c1e5033891f5c7ad
jagdishlove <jagdish.funn@gmail.com>
2022-01-15 15:48:32 +0530

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,89 @@
-import "./App.css";
 import io from "socket.io-client";
 import React, { useState } from "react";
+import Chat from "./Chat";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Fade from "@mui/material/Fade";
+
 const socket = io.connect("http://localhost:3001");
+
+const useStyle = makeStyles({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    itemAlign: "center",
+    justifyContent: "center",
+    gap: "1em",
+  },
+});
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  const [error, setError] = useState(false);
+  const classes = useStyle();
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
-    }
-    else {
-
-      return alert("please fill all fields")
+      setShowChat(true);
+    } else {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
     }
   };
   return (
-    <div className="App">
-      <h4>Join A Chat</h4>
-      <input
-        type="text"
-        placeholder="Enter name"
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <input type="text" placeholder="Room ID.."
-        onChange={(e) => {
-          setRoom(e.target.value);
-        }} />
-      <button onClick={joinRoom}>Join A Room</button>
-
-      {username}
-      {room}
-    </div>
+    <Box>
+      {!showChat ? (
+        <Box className={classes.form} width="50%" margin="10% auto">
+          <Typography
+            fontSize={{
+              lg: 40,
+              md: 30,
+              sm: 25,
+              xs: 28,
+            }}
+            sx={{ fontWeight: "bold", margin: "auto" }}
+            variant="h3"
+          >
+            Live Chat
+          </Typography>
+          <TextField
+            label="Enter name"
+            variant="outlined"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <TextField
+            label="Enter Room ID.."
+            variant="outlined"
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
+          <Button variant="contained" onClick={joinRoom}>
+            Join A Room
+          </Button>
+          {error && (
+            <Fade in={error}>
+              <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                Please fill the all field
+              </Alert>
+            </Fade>
+          )}
+        </Box>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+      {/* <Chat socket={socket} username={username} room={room} /> */}
+    </Box>
   );
 }
 
