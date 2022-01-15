0afccea6192ed7470afd58e23282cc7dcb2001d4
jagdishlove <jagdish.funn@gmail.com>
2022-01-15 16:54:04 +0530

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import React, { useState } from "react";
 import Chat from "./Chat";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, AppBar, Container, Toolbar } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
@@ -38,52 +38,65 @@ function App() {
     }
   };
   return (
-    <Box>
-      {!showChat ? (
-        <Box className={classes.form} width="50%" margin="10% auto">
-          <Typography
-            fontSize={{
-              lg: 40,
-              md: 30,
-              sm: 25,
-              xs: 28,
-            }}
-            sx={{ fontWeight: "bold", margin: "auto" }}
-            variant="h3"
-          >
-            Live Chat
-          </Typography>
-          <TextField
-            label="Enter name"
-            variant="outlined"
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-          <TextField
-            label="Enter Room ID.."
-            variant="outlined"
-            onChange={(e) => {
-              setRoom(e.target.value);
-            }}
-          />
-          <Button variant="contained" onClick={joinRoom}>
-            Join A Room
-          </Button>
-          {error && (
-            <Fade in={error}>
-              <Alert severity="error">
-                <AlertTitle>Error</AlertTitle>
-                Please fill the all field
-              </Alert>
-            </Fade>
-          )}
-        </Box>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
-      {/* <Chat socket={socket} username={username} room={room} /> */}
-    </Box>
+    <>
+      <Box >
+        {!showChat ? (
+          <Box className={classes.form} width="50%" margin="10% auto">
+            <Typography
+              fontSize={{
+                lg: 40,
+                md: 30,
+                sm: 25,
+                xs: 28,
+              }}
+              sx={{ fontWeight: "bold", margin: "auto" }}
+              variant="h3"
+            >
+              Live Chat
+            </Typography>
+            <TextField
+              label="Enter name"
+              variant="outlined"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <TextField
+              label="Enter Room ID.."
+              variant="outlined"
+              onChange={(e) => {
+                setRoom(e.target.value);
+              }}
+            />
+            <Button variant="contained" onClick={joinRoom}>
+              Join A Room
+            </Button>
+            {error && (
+              <Fade in={error}>
+                <Alert severity="error">
+                  <AlertTitle>Error</AlertTitle>
+                  Please fill the all field
+                </Alert>
+              </Fade>
+            )}
+          </Box>
+        ) : (
+          <Chat socket={socket} username={username} room={room} />
+        )}
+
+      </Box>
+      <Box>
+        <AppBar sx={{ position: 'sticky' }} color="primary">
+          <Container maxWidth>
+            <Toolbar>
+              <Typography variant="body1" color="inherit">
+                © 2022 Owner Jagdish
+              </Typography>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
+    </>
   );
 }
 
